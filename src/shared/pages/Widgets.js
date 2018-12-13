c5e49c0a69de3ef77531cad7d4ee4a05873d639b
AGoodnight <chaofoam@gmail.com>
2018-12-13 18:36:21 -0500

    Initial commit

---
+++ src/shared/pages/Widgets.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import Card from '../components/card'
+
+class Widgets extends Component {
+
+  constructor(props) {
+
+    super(props)
+
+    let widgets
+
+    if (__isBrowser__) {
+      widgets = window.__INITIAL_DATA__
+      delete window.__INITIAL_DATA__
+    } else {
+
+      widgets = props.staticContext
+    }
+
+    this.state = {
+      loading: widgets && widgets.hasOwnProperty('data') ? false : true,
+      widgets
+    }
+
+    this.fetchWidgets = this.fetchWidgets.bind(this)
+  }
+
+  componentDidMount() {
+
+    const { loading, widgets } = this.state
+
+    if (loading) {
+      this.fetchWidgets()
+    }
+  }
+
+  fetchWidgets() {
+
+    const { fetchInitialData } = this.props
+    this.setState(() => ({loading: true}))
+
+    fetchInitialData()
+    .then(widgets => this.setState({
+      loading: false,
+      widgets
+    }))
+  }
+
+  render() {
+    let totalWidgets = 0;
+    const { loading, widgets } = this.state
+    console.log(this.state)
+
+    if (loading === true) {
+      return (<p>'Loading...'</p>)
+    }else{
+      totalWidgets = widgets.data.length
+      return (
+        <div>
+          {loading && totalWidgets === 0 && <h2>Loading...</h2>}
+          {!loading && totalWidgets === 0 && <h2>Empty.</h2>}
+          <Card apps={widgets.data} totalapps={totalWidgets} />
+        </div>
+      )
+    }
+  }
+}
+
+export default Widgets
---
+++ src/shared/reducers/appsReducer.js
@@ -0,0 +1,20 @@
+import { Constants as constants } from '../constants/index'
+import initialState from './initialState'
+
+const appsReducer = ( state = initialState.apps, action) => {
+  switch (action.type) {
+    case constants.ACTIONS.APPS.REQUEST_APPS:
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+    case constants.ACTIONS.APPS.RECEIVE_APPS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        apps: action.apps
+      });
+    default:
+      return state
+  }
+}
+
+export default appsReducer
